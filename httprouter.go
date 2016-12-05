fcf2c2613fe901d75aea4776b660a6d37b87328b
Michael Prinsloo <mike.prinsloo@gmail.com>
2016-12-05 20:44:25 +0200

    Added a description field to routes.

--- httprouter.go
+++ httprouter.go
@@ -10,14 +10,15 @@ type Route struct {
 	Label       string           `json:"label"`
 	Method      string           `json:"method"`
 	Pattern     string           `json:"pattern"`
+	Description string           `json:"description"`
 	HandlerFunc http.HandlerFunc `json:"-"`
 }
 
 type Routes []Route
 
 // Add a new Route to Routes.
-func (r *Routes) Add(label, method, pattern string, handlerFunc http.HandlerFunc) {
-	r = append(r, Route{label, method, pattern, handlerFunc})
+func (r *Routes) Add(label, method, pattern, description string, handlerFunc http.HandlerFunc) {
+	r = append(r, Route{label, method, pattern, description, handlerFunc})
 }
 
 // Create a new router.
